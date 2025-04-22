3d02539c0d673d323454994a14636537dd7ef5ee
krishna <krishna.vennapusa@gmail.com>
2025-04-22 21:13:56 +0530

    99% Practice

--- Scripting/9.array.sh
+++ Scripting/9.array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
- FRUITS=("Apple","Bananna","Mongo") #array the index value starts from 0
+ FRUITS=("Apple" "Bananna" "Mongo") #array the index value starts from 0
 
  echo "First value: ${FRUITS[0]}"
- echo "Second value ${FRUITS[1]}"
- echo "Third value ${FRUITS[2]}"
- echo "Print the value ${FRUITS[@]}" #@ prints the all values
+ echo "Second value: ${FRUITS[1]}"
+ echo "Third value: ${FRUITS[2]}"
+ echo "Print the value: ${FRUITS[@]}" #@ prints the all values
